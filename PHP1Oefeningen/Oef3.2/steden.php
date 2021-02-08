044d2c4c8e0a50615a8b2d998a8ecac8d907564c
larskerstens <larskerstens10@gmail.com>
2021-02-08 20:36:29 +0100

    PHP1oef Oef3.2 add

---
+++ PHP1Oefeningen/Oef3.2/steden.php
@@ -0,0 +1,29 @@
+<?php
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+require_once "lib/autoload.php";
+
+PrintHead();
+PrintJumbo();
+?>
+
+<div class="container">
+    <div class="row">
+        <?php
+
+        //get data
+        $rows = GetData( "select * from images" );
+
+        //get template
+        $template = file_get_contents("templates/column.html");
+
+        //merge
+        $html = MergeViewWithData( $template, $rows );
+        print $html;
+        ?>
+    </div>
+</div>
+
+</body>
+</html>
