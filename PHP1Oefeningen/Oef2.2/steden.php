1c99f9a04883a88d69929537a574f5e65968a00e
larskerstens <larskerstens10@gmail.com>
2021-02-08 13:25:58 +0100

    PHP1oef Oef3.1 add

--- PHP1Oefeningen/Oef2.2/steden.php
+++ PHP1Oefeningen/Oef2.2/steden.php
@@ -12,22 +12,16 @@ PrintJumbo();
 <div class="container">
     <div class="row">
         <?php
+
+        //get data
         $rows = GetData( "select * from images" );
-        //$counter = 0;
-        foreach ($rows as $row) {
-            //$counter++;
-            $row["img_location"] = ucfirst($row["img_location"]);
-            $row["img_title"] = ucfirst($row["img_title"]);
-            $link_image = "img/" . $row['img_filename'];
-            echo '<div class="col-sm-4">';
-            echo '<h3>'.$row["img_title"].'</h3>';
-            echo "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At doloribus explicabo.</p>";
-            echo '<p>'.$row["img_width"].' x '.$row["img_height"].' pixels'.'</p>';
-            echo '<h4>'.'Location: '.$row["img_filename"].'</h4>';
-            echo '<h5>'.'Place: '.$row["img_location"].'</h5>';
-            echo '<figure><div class="img-hover-zoom img-hover-zoom--colorize"><img src="' . $link_image . '"></div>
-            <figcaption><p><a class="info" href=stad.php?img_id=' . $row['img_id'] . '>Meer info over deze foto -></a></p></figcaption></figure></div>';
-        }
+
+        //get template
+        $template = file_get_contents("templates/column.html");
+
+        //merge
+        $html = MergeViewWithData( $template, $rows );
+        print $html;
         ?>
     </div>
 </div>
