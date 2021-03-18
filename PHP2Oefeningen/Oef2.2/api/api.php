b56c372941bc919c8bb91bdffb6988a2ec9eb9d1
larskerstens <larskerstens10@gmail.com>
2021-03-18 11:14:27 +0100

    2.2 done

--- PHP2Oefeningen/Oef2.2/api/api.php
+++ PHP2Oefeningen/Oef2.2/api/api.php
@@ -1,7 +1,10 @@
 <?php
+$public_access = true;
+require_once "../lib/autoload.php";
+
 //Allow access from outside (see CORS)
 //header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Origin: 'https://gf.dev'");
+header("Access-Control-Allow-Origin: 'https://localhost'");
 header("Access-Control-Allow-Credentials 'true'");
 
 
@@ -27,6 +30,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 $request_uri = $_SERVER['REQUEST_URI'];
 
 $parts = explode("/", $request_uri);
+$dbManager = $container->getDBManager();
 
 //zoek "rest" in de uri
 for ( $i=0; $i<count($parts) ;$i++)
@@ -40,49 +44,54 @@ for ( $i=0; $i<count($parts) ;$i++)
 $request_part = $parts[$i+2];
 if ( count($parts) > $i + 3 ) $id = $parts[$i + 3];
 
-//GET spelers: alle spelers geven
-if ( $method == "GET" AND $request_part == "spelers" )
+//alle btw code
+if ( $method == "GET" AND $request_part == "btwcodes" )
 {
-    $sql = "select * from spelers";
+    $sql = "select * from eu_btw_codes";
     // ... execute $sql
-    print json_encode( [ "msg" => $sql ] ) ; //normaal zou je hier alle spelers teruggeven
+    print json_encode( $dbManager->getData($sql) ) ;
 }
 
-//GET speler: één speler geven
-if ( $method == "GET" AND $request_part == "speler" )
+//één btw code
+if ( $method == "GET" AND $request_part == "btwcode" )
 {
-    $sql = "select * from spelers where spe_id=$id";
+    $sql = "select * from eu_btw_codes where eub_id='$id'";
     // ... execute $sql
-    print json_encode( [ "msg" => $sql ] ) ; //normaal zou je hier één speler teruggeven
+    print json_encode( $dbManager->getData($sql) ) ;
 }
 
-//POST spelers: een speler toevoegen
-if ( $method == "POST" AND $request_part == "spelers"  )
-{
-    $naam = $_POST["naam"];
-    $sql = "INSERT INTO spelers SET spe_naam='$naam' ";
-    // ... execute $sql
-    http_response_code(201);
-    print json_encode( [ "msg" => $sql ] ) ; //normaal zou je hier een OK teruggeven
+//update speler
+if ($method == "PUT" and $request_part == "btwcode") {
+    $sql = "select * from eu_btw_codes where eub_id=$id";
+    $data = $container->getDBManager()->GetData( $sql , 'assoc' );
+    if ($data) {
+        $contents = json_decode(file_get_contents("php://input"));
+        $newcode = $contents->code;
+        $newland = $contents->land;
+
+        $sql = "UPDATE eu_btw_codes SET eub_land = '$newland', eub_code = '$newcode' WHERE eub_id = $id";
+        $data = $container->getDBManager()->ExecuteSQL($sql);
+        print json_encode(["msg" => "gelukt"]);
+        exit;
+    } else {
+        print json_encode(["msg" => 'mislukt']);
+        exit;
+    }
 }
 
-//PUT speler: een speler updaten
-if ( $method == "PUT" AND $request_part == "speler" )
+//delete btw code
+if ( $method == "DELETE" AND $request_part == "btwcode" )
 {
-    $contents = json_decode( file_get_contents("php://input") );
-    $newdata = $contents->naam;
-
-    $sql = "UPDATE spelers SET spe_naam='$newdata' WHERE spe_id=$id";
+    $sql = "DELETE FROM eu_btw_codes WHERE eub_id='$id'";
     // ... execute $sql
-    print json_encode( [ "msg" => $sql ] ) ; //normaal zou je hier een OK teruggeven
-}
+    $result = $dbManager->executeSQL($sql);
 
-//DELETE speler: een speler verwijderen
-if ( $method == "DELETE" AND $request_part == "speler" )
-{
-    $sql = "DELETE FROM spelers WHERE spe_id=$id";
-    // ... execute $sql
-    print json_encode( [ "msg" => $sql ] ) ; //normaal zou je hier een OK teruggeven
+    if ($result->rowCount() > 0){
+        print json_encode( [ "BTW Code deleted" ] ) ;
+    } else {
+        print json_encode( [ "Error: no delete" ] ) ;
+    }
+
 }
 
 ?>
